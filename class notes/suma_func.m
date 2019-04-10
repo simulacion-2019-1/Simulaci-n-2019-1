6fd905ad7e633430ed41df681ab5b39aba1ff25e
faiverht <40344999+faiverht@users.noreply.github.com>
2019-04-10 07:52:44 -0500

    Sesión Abril10

---
+++ class notes/suma_func.m
@@ -0,0 +1,10 @@
+function [a,b] = suma_func(x,y)
+% Ejemplo de crear función
+%   EJ:  [a,b] = suma_func(x,y)
+%       a retorna la suma y b la resta
+%   copyright@ ---- udfjdc
+a = x+y;
+b = x-y;
+%  Okkkk
+end
+
